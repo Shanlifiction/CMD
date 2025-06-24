bd81e066a6a193f7bd04a10bfea69876267a6457
Shanli Hojjati <shanli.hojjati@gmail.com>
2025-06-24 19:57:38 +0200

    hello

--- cmd.py
+++ cmd.py
@@ -2,7 +2,7 @@
 #global var tex current_dir
 #global var
 #global var
-current_dir = ""
+current_dir = "" #current_dir = "" #platsposition
 
 #dics (giltiga commandon) key: CD & value: func ->
 #dics
@@ -10,10 +10,11 @@ current_dir = ""
 
 #class (error handle)
 class HandleAll:
-    def __init__(self):
-        self.command_input = ""
+    def __init__(self, command):
+        self.command_input = command
+        self.command_split = [] #index [0] är commandot
         self.valid_commands = {
-            "dir": 0,    #0=func soon (utan parantes)
+            "dir": (0, (1, 2, 3)),    #0=func soon (utan parantes)
             "cd": 1,
             "cd..": 2,
             "cd/": 3,
@@ -34,8 +35,44 @@ class HandleAll:
             "q": 13, #same
         }
 
-    def process_input():
-        ...
+    def command_exists(self):
+        if self.command_split[0] in self.valid_commands: #finns command i dicen?
+            return True
+        return False #annars avsluta och meddela 
+    
+    def command_valid_syntax(self):
+        return True
+    #innehållet stämmer?
+    #antal argument är korrekt (cd är solo, medan mkdir skapar (kan gå ihop med fler)
+    #skrivet på korrekt sätt
+    
+    def command_valid_argzzz(self):
+        return True
+    #argument (tex mappen) existerar osv
+
+
+    def process_input(self):
+        if self.command_input.strip() == "": #kollar om command har innehåll och tar bort ev mellanslag
+            print("Please enter valid command.")
+            return
+        
+        #split command into a list
+        self.command_split = self.command_input.split() 
+
+        if not self.command_exists(): #if sats som kollar om commandot EJ finns
+            print("Command does not exist.")
+            return
+
+        if not self.command_valid_syntax():
+            print("Invalid syntax.")
+            return
+        
+        if not self.command_valid_argzzz():
+            print("Here we go again.")
+            return
+
+        print(self.command_input, " -> ", self.command_split)
+
         #try/except
         #OBS    
         #valid path     #tex "move" finns commandot? & "fil.txt" "mapp/fil.txt" finns dessa två argument?
@@ -44,8 +81,10 @@ class HandleAll:
 def main():
     while True:
         command_input = (input(f"{current_dir}>")) #det är en string
-    #current_dir = "" #platsposition
+        command1 = HandleAll(command_input)
+        command1.process_input()
 
+    
 main()
 
 
