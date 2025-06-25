162153e5118041a0b7b95fc1dc0f934158bcc2ce
Hampus Söderberg <hampalexo@gmail.com>
2025-06-25 09:33:37 +0200

    brancinh

--- cmd.py
+++ cmd.py
@@ -10,8 +10,9 @@ current_dir = ""
 
 #class (error handle)
 class HandleAll:
-    def __init__(self):
-        self.command_input = ""
+    def __init__(self, command):
+        self.command_input = command
+        self.command_split = []
         self.valid_commands = {
             "dir": 0,    #0=func soon (utan parantes)
             "cd": 1,
@@ -34,8 +35,37 @@ class HandleAll:
             "q": 13, #same
         }
 
-    def process_input():
-        ...
+    def command_exists(self):
+        if self.command_split[0] in self.valid_commands:
+            return True
+        return False
+    
+    def command_valid_syntax(self):
+        return True
+
+    def process_input(self):
+        # '   '
+        # ''
+        # 'cd C:\'  ->  ["cd", "C:\"]
+        if self.command_input.strip() == "":
+            print("Please enter valid command.")
+            return
+        
+        # split command into list, where [0] is command 
+        self.command_split = self.command_input.split()
+        
+        if not self.command_exists():
+            print("Command does not exist.")
+            return
+        
+        if not self.command_valid_syntax():
+            print("Invalid syntax.")
+            return
+        
+        print(self.command_input, " -> ", self.command_split)
+
+
+
         #try/except
         #OBS    
         #valid path     #tex "move" finns commandot? & "fil.txt" "mapp/fil.txt" finns dessa två argument?
@@ -44,11 +74,10 @@ class HandleAll:
 def main():
     while True:
         command_input = (input(f"{current_dir}>")) #det är en string
-    #current_dir = "" #platsposition
+        command1 = HandleAll(command_input)
+        command1.process_input()
+
+
+        #current_dir = "" #platsposition
 
 main()
-
-
-
-
-
