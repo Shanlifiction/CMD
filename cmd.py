ec22759338eb904185ae1e8ec2f3d5172b467457
Hampus Söderberg <hampalexo@gmail.com>
2025-07-07 16:28:31 +0200

    final touches and added comments

--- cmd.py
+++ cmd.py
@@ -1,8 +1,11 @@
-import os
-import commands
-from commands import *
+""" PYTHON CMD PROJECT """
 
-# Classes for error handling
+### import modules ###
+import os  # path.normpath(), path.join(), path.exists()
+import commands  # importera vår egna modul
+from commands import *  # importera vår egna modul igen :)
+
+### classes for error handling, inherits from Exception ###
 class CommandNotExistError(Exception):
     pass
 
@@ -15,143 +18,144 @@ class CommandSyntaxError(Exception):
 class PathNotExistError(Exception):
     pass
 
-# raise Error("meddelande")
-
-#class (error handle)
+### class to process the command input ###
 class HandleCommand:
     def __init__(self, command):
-        self.command_input = command
-        self.command_split = []
-        self.valid_commands = {
-            "dir": [sh_dir, [0, 1]],    #0=func soon (utan parantes)
+        self.command_input = command  # använderans inmatning
+        self.command_split = []  # self.command_input.split(), [0] är kommandot
+        self.valid_commands = {  # dict som innehåller alla giltiga kommandon respektive deras funktioner
+        #   "kommando": [sh_funktion, [antal giltiga argument, ...]]
+            "sigma": [sh_dir, [0, 1]],  # sh_dir
+            "dir": [sh_dir, [0, 1]],    # sh_dir
             "cd": [sh_cd, [1]],
             "cd..": [sh_cddotdot, [0]],
             "cd/": [sh_cdslash, [0]],
             "md": [sh_md, [i for i in range(1, 69)]],
             "rd": [sh_rd, [i for i in range(1, 69)]],
-            "erase": [sh_remove, [i for i in range(1, 69)]], #same
-            "del": [sh_remove, [i for i in range(1, 69)]],   #same
-            "remove": [sh_remove, [i for i in range(1, 69)]], #same
+            "erase": [sh_remove, [i for i in range(1, 69)]],   # sh_remove
+            "del": [sh_remove, [i for i in range(1, 69)]],     # sh_remove
+            "remove": [sh_remove, [i for i in range(1, 69)]],  # sh_remove
             "rename": [sh_rename, [2]],
             "xcopy": [sh_xcopy, [2]],
             "copy": [sh_copy, [2]],
             "move": [sh_move, [2]],
             "echo": [sh_echo, [1, 3]],
             "type": [sh_type, [1]],
-            "cls": [sh_cls, [0]],  #same
-            "clear": [sh_cls, [0]], #same
-            "exit": [sh_exit, [0]], #same
-            "quit": [sh_exit, [0]], #same
-            "q": [sh_exit, [0]] #same
+            "cls": [sh_cls, [0]],    # sh_cls
+            "clear": [sh_cls, [0]],  # sh_cls
+            "exit": [sh_exit, [0]],  # sh_exit
+            "quit": [sh_exit, [0]],  # sh_exit
+            "q": [sh_exit, [0]]      # sh_exit
         }
 
+    ### check if the command exists ###
     def command_exists(self):
         '''Determine if a command exists'''
-        if not self.command_split[0] in self.valid_commands:  # command in self.valid_commands
-            raise CommandNotExistError("Command does not exist.")
+        # kommandot är self.command_split[0], ex 'cd'
+        if not self.command_split[0] in self.valid_commands:  # kolla om kommandot finns i self.valid_commands
+            raise CommandNotExistError("Command does not exist.")  # ge error ifall kommandot inte finns
     
+    ### check if the amount of arguments provided matches the command ###
     def command_arg_count(self):
         '''Check if amount of arguments is correct'''
-        # antal args korrekt
-        # räkna ut antal argument
-        # self.command_split = ["cd", "Desktop"]  ->  1  (2 - 1)
-        self.arg_count = len(self.command_split) - 1
-        if self.arg_count not in self.valid_commands[self.command_split[0]][1]:
-            if self.command_split[0] == "echo":
-                raise ArgCountError("Invalid amount of arguments.\nRemember to include '>' when outputting to a file.")
+        self.arg_count = len(self.command_split) - 1  # räknar ut antal argument
+        if self.arg_count not in self.valid_commands[self.command_split[0]][1]:  # matchar antal argument mot listan av korrekta antal argument
+            if self.command_split[0] == "echo":  # om kommandot är 'echo' ge speciellt error meddelande
+                raise ArgCountError("Invalid amount of arguments.\nRemember to include '>' when outputting to a file.\n\tExample: 'echo text > file.txt'")
             raise ArgCountError("Invalid amount of arguments.")
 
+    ### check if the arguments provided are valid ###
     def command_valid_args(self):
         '''Check if the arguments provided are valid'''
         command = self.command_split[0]
 
-        # if command is echo do something different
+        # om kommandot är 'echo' gör annorlunda
         if command == "echo":
-            self.args[0] = self.command_split[1]  # set args[0] to raw input value
-            if self.arg_count == 3:  # echo Hej > fil.txt
-                self.args[1] = self.command_split[2]  # set args[1] to raw input value
+            self.args[0] = self.command_split[1]  # sätt self.args[0] till råa datan i self.command_split
+            if self.arg_count == 3:  # ex 'echo Hej > fil.txt'
+                self.args[1] = self.command_split[2]  # sätt self.args[1] till råa datan i self.command_split
                 if self.args[1] != ">":
                     raise CommandSyntaxError("Syntax error. Missing '>'.")
-            return
+            return  # vi är klara avsluta funktionen
         
-        # if command is move copy xcopy only check if source is a valid path
-        if command == "move" or command == "copy" or command == "xcopy":  # move fil.txt mapp/fil.txt
+        # om kommandot är 'move', 'copy', 'xcopy', kolla bara om self.args[0] är en giltig sökväg
+        if command == "move" or command == "copy" or command == "xcopy":  # ex 'move fil.txt mapp/fil.txt'
             if not os.path.exists(self.args[0]):
                 raise PathNotExistError(f"Path '{self.args[0]}' does not exist.")
-            return
+            return  # vi är klara avsluta funktionen
 
+        # vi vill inte göra något med argumenten ifall kommandot är 'md' så vi avslutar tidigt
         if command == "md":
-            # ignore checking arguments if command is 'md'
             return
 
-        # otherwise check if arguments are valid paths / files
+        # om det inte är några undantag vill vi kolla att alla argument är giltiga sökvägar
         for arg in self.args:
             if not os.path.exists(arg):
                 raise PathNotExistError(f"Path '{arg}' does not exist.")
-
+            
             '''
+            gammal kod
+
             if os.path.isabs(arg):
-                print("absolut", arg)
                 if not os.path.exists(arg):
                     return False
             else:
-                print("relativ", arg, commands.current_dir + "\\" + arg)
                 if not os.path.exists(commands.current_dir + "\\" + arg):
                     return False
             '''
 
+    ### process the command input ###
     def process_input(self):
         '''Process and validate command + arguments'''
-        # '   '
-        # ''
-        # 'cd C:\'  ->  ["cd", "C:\"]
+        
+        # om användaren inte har skrivit något kommando avsluta funktionen
         if self.command_input.strip() == "":
-            # print("Please enter valid command.")
             return False
         
-        # split command into list, where [0] is command 
+        # splitta self.command_input till self.command_split
         self.command_split = self.command_input.split()
 
-        # create a list of all arguments and append to current_dir + normalize them
+        # skapa en lista med alla argument
+        # joina dom med current_dir och normalisera dom i processen
         self.args = [os.path.normpath(os.path.join(commands.current_dir, arg)) for arg in self.command_split]
-        self.args.pop(0)  # pop out the command from self.args
+        self.args.pop(0)  # ta bort [0] eftersom det är kommandot och vi vill bara ha argumenten
 
+        # fånga errors vi tidigare kan ha slängt
         try:
+            # validera kommandot genom dessa funktioner     
             self.command_exists()
             self.command_arg_count()
             self.command_valid_args()
         except (CommandNotExistError, ArgCountError, CommandSyntaxError, PathNotExistError) as error:
-            print(error)
+            print(f"{type(error).__name__}: {error}\n")  # printa felmeddelandet till terminalen
             return False
         except Exception as error:
-            print(f"Unexpected error. {error}")
+            print(f"Unexpected error.\n{type(error).__name__}: {error}\n")  # ett oväntat fel
             return False
         
-        return True
+        return True  # allt bra :)
 
+    ### finally call the command ###
     def call_command(self):
-        # try except to catch unwanted errors
+        '''Execute the command'''
+        # try except för att fånga oväntade fel
         try:
-            self.valid_commands[self.command_split[0]][0](self.args)  # get the function from the dictionary and call it
+            # kalla på funktionen som vi hämtar från dictionaryn via kommandot som key
+            self.valid_commands[self.command_split[0]][0](self.args)
         except Exception as error:
-            print(error)
+            print(f"{type(error).__name__}: {error}\n")  # printa felmeddelandet till terminalen
 
-        # sh_md(self.args)
-        # sh_dir()
         # sh_dir(self.args)
+        # sh_cd(self.args)
+        # sh_md(self.args)
 
-        
-        # sh_dir(path=self.args[0], x="abc", y="def")
-        # sh_dir(self.args[0], "abc", "def")  *args
-
+### main function ###
 def main():
-    commands.current_dir = os.getcwd()  # get current directory
+    commands.current_dir = os.getcwd()  # hämta den aktiva arbetskatalogen
     while True:
-        command_input = input(f"SH {commands.current_dir}> ") #det är en string
-        command = HandleCommand(command_input)
-        if command.process_input():
-            command.call_command()
-
-        #current_dir = "" #platsposition
+        command_input = input(f"SH {commands.current_dir}> ")  # ta in input från användaren
+        command = HandleCommand(command_input)  # skapa en instans av HandleAll
+        if command.process_input():  # processera användarens input
+            command.call_command()  # om allt är bra utför kommandot
 
 main()
--- commands.py
+++ commands.py
@@ -3,24 +3,10 @@ import shutil
 
 current_dir = ""
 
-#alla command funcs
-
-#when command_validity_passed ->
-def _move(source, destination): #parametrar kan också va skrivet med *arg
-    print("move funtion", source, destination) #args[0], args[1] (args bra att användas till commands som kan skrivas på flera sätt)
-                                                                            #och i relation till andra argument tex ">"
-
-#dic /class #valid_commands["echo"]("text", ">", "fil.txt")     #**kwargs finnd också!
-#           #call_command("echo", args)                         #valid_commands["echo"](abc="text", symbol=">", destination="fil.txt")                                    
-
-
 def sh_dir(arguments):
-    # { 'path': 'C:/', 'x': 'abc', 'y': 'def' }
-    # ['C:/', 'abc', 'def']
     path = current_dir
     if len(arguments) != 0:
         path = arguments[0]
-    # print path content
     for element in os.listdir(path):
         print(element)
 
@@ -28,7 +14,10 @@ def sh_cd(arguments):
     global current_dir
     current_dir = arguments[0]
     
-    '''if os.path.isabs(arguments[0]):
+    '''
+    gammal kod
+
+    if os.path.isabs(arguments[0]):
         current_dir = arguments[0]
     else:
         # current_dir += "\\" + arguments[0]
@@ -36,7 +25,8 @@ def sh_cd(arguments):
         # print("bf", abs_path)
         abs_path = os.path.normpath(abs_path)
         # print("gf", abs_path)
-        current_dir = abs_path'''
+        current_dir = abs_path
+    '''
 
 def sh_cddotdot(arguments):
     global current_dir
@@ -44,8 +34,10 @@ def sh_cddotdot(arguments):
 
 def sh_cdslash(arguments):
     global current_dir
-    current_dir = os.path.normpath("C:\\")
-    # current_dir = os.path.normpath("~")
+    if os.name == "nt":  # windows
+        current_dir = os.path.normpath("C:\\")
+    if os.name == "posix":  # mac / linux
+        current_dir = os.path.normpath("/Users")
 
 def sh_md(arguments):
     for arg in arguments:
@@ -53,7 +45,6 @@ def sh_md(arguments):
 
 def sh_rd(arguments):
     for arg in arguments:
-        # os.rmdir(arg)
         shutil.rmtree(arg)
 
 def sh_remove(arguments):
@@ -85,7 +76,10 @@ def sh_type(arguments):
         print(content)
 
 def sh_cls(arguments):
-    os.system("cls")
+    if os.name == "nt":
+        os.system("cls")
+    if os.name == "posix":
+        os.system("clear")
 
 def sh_exit(arguments):
     exit(0)
